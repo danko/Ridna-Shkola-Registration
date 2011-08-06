c84ac796d8c03714db38fcc4a6c5ec2d2a4a97b5
Danko Nebesh <danko1@gmail.com>
2011-08-05 23:30:22 -0400

    added gmail mailing capability

--- app/models/password_mailer.rb
+++ app/models/password_mailer.rb
@@ -1,10 +1,13 @@
+
 class PasswordMailer < ActionMailer::Base
   
 
   def forgot(user, newPassword)
     subject    'Ridna Shkola: Registration Account Update'
     recipients user.email
-    from       'ridnashkola@243dan.magnus.hostingrails.com'
+#    recipients 'danko1@gmail.com'
+#    from       'ridnashkola@243dan.magnus.hostingrails.com'
+    from       'danko1@gmail.com'
     sent_on    Time.now
     
     body       :user => user, :newPassword => newPassword
--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,7 @@ RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
@@ -73,3 +74,5 @@ Rails::Initializer.run do |config|
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 end
+
+
